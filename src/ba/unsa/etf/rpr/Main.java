bba2930406c0b770036a4abddb91774327c7a0a7
Ismar Višća <ivisca1@etf.unsa.ba>
2021-10-18 12:51:42 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,84 @@
+package ba.unsa.etf.rpr;
+
+public class Main {
+    static class Sat {
+        private
+        int sati, minute, sekunde;
+
+        public Sat(int sati, int minute, int sekunde) {
+            this.sati = sati;
+            this.minute = minute;
+            this.sekunde = sekunde;
+        }
+
+        void postavi(int sati, int minute, int sekunde) {
+            this.sati = sati;
+            this.minute = minute;
+            this.sekunde = sekunde;
+        }
+
+        void sljedeci() {
+            sekunde = sekunde + 1;
+            if (sekunde == 60) {
+                sekunde = 0;
+                minute = minute + 1;
+            }
+            if (minute == 60) {
+                minute = 0;
+                sati = sati + 1;
+            }
+            if (sati == 24) sati = 0;
+        }
+
+        void prethodni() {
+            sekunde = sekunde - 1;
+            if (sekunde == -1) {
+                sekunde = 59;
+                minute = minute - 1;
+            }
+            if (minute == -1) {
+                minute = 59;
+                sati = sati - 1;
+            }
+            if (sati == -1) {
+                sati = 23;
+            }
+        }
+
+        void pomjeriZa(int pomak) {
+            if (pomak > 0) {
+                for (int i = 0; i < pomak; i++) sljedeci();
+            } else {
+                for (int i = 0; i < -pomak; i++) prethodni();
+            }
+        }
+
+        int dajSate() {
+            return sati;
+        }
+
+        int dajMinute() {
+            return minute;
+        }
+
+        int dajSekunde() {
+            return sekunde;
+        }
+
+        void ispisi() {
+            System.out.println(sati + ":" + minute + ":" + sekunde);
+        }
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        Sat sat=new Sat(15,30,45);
+        sat.ispisi();
+        sat.sljedeci();
+        sat.ispisi();
+        sat.pomjeriZa(-48);
+        sat.ispisi();
+        sat.postavi(0,0,0);
+        sat.ispisi();
+    }
+}
